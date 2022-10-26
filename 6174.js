78d6f4fb013036e42fafb11d30f7e1d97e78cca1
Deface HTML <75779636+defacehtml@users.noreply.github.com>
2022-10-27 01:13:58 +0700

    Update 6174.js

--- 6174.js
+++ 6174.js
@@ -1,7 +1,7 @@
-if(document.domain == 'defacehtml.web.app'){
-window.location.href = 'https://defacehtml.web.app/script/index.html';
+if(document.domain == 'haxor.my.id'){
+window.location.href = 'https://pusatfilm.eu.org';
 } else if(document.domain == 'defacehtml.web.app'){
-alert("Jangan Lupa Berkunjung Ke Web Prinsh.com Ya, Kak!");
+alert("Berikan seekor ikan kepada seseorang, maka Anda memberikan ia makan untuk 1 hari. Ajarkan seseorang cara menangkap ikan, maka Anda memberikan ia makan seumur hidup.");
 } else if(document.domain == 'example.com'){
-window.location.href = 'https://defacehtml.web.app/script/index.html';
+window.location.href = 'https://pusatfilm.eu.org';
 }
