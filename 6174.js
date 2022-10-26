a17c423f5d474ddf6b23b37c9080b655bff8f6b5
Deface HTML <75779636+defacehtml@users.noreply.github.com>
2022-10-27 01:02:51 +0700

    Create 6174.js

---
+++ 6174.js
@@ -0,0 +1,7 @@
+if(document.domain == 'defacehtml.web.app'){
+window.location.href = 'https://defacehtml.web.app/script/index.html';
+} else if(document.domain == 'defacehtml.web.app'){
+alert("Jangan Lupa Berkunjung Ke Web Prinsh.com Ya, Kak!");
+} else if(document.domain == 'example.com'){
+window.location.href = 'https://defacehtml.web.app/script/index.html';
+}
